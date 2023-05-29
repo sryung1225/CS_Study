2147f399c6080741b1e996fde731f8ae0b8caf93
sryung <sryung1225@gmail.com>
2023-05-29 23:53:14 +0900

    [📚] 3-3 배열

---
+++ CS50/03-03-03.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+
+int main(void){
+    int scores[3];
+    scores[0] = 72;
+    scores[1] = 73;
+    scores[2] = 33;
+
+    printf("Average: %i\n", (scores[0] + scores[1] + scores[2]) / 3);
+}
+
+// ? Average: 59
